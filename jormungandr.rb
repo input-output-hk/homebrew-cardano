0faed83eab8acf22bf8129c781a763d3d365a8d8
Samuel Leathers <samuel.leathers@iohk.io>
2019-06-17 07:17:02 -0400

    fix jormungandr url and hash

--- jormungandr.rb
+++ jormungandr.rb
@@ -1,10 +1,10 @@
 class Jormungandr < Formula
-  version '0.2.1'
+  version 'v0.2.1'
   desc "Cardano Node on Rust"
   homepage "https://github.com/input-output-hk/jormungandr"
 
   url "https://github.com/input-output-hk/jormungandr/releases/download/#{version}/jormungandr-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "32754b4173ac87a7bfffd436d601a49362676eb1841ab33440f2f49c002c8967"
+  sha256 "5c5309c9f3b5ba2a36b3072f7e9116d953f572995edc9194a504718272f3a53c"
 
   def install
     bin.install "jormungandr"
