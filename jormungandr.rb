1f6617d26cf603d88f1bb4fc61de6a564ff45027
Samuel Leathers <samuel.leathers@iohk.io>
2019-07-12 16:07:03 -0400

    jormungandr: 0.2.4 -> 0.3.0

--- jormungandr.rb
+++ jormungandr.rb
@@ -1,10 +1,10 @@
 class Jormungandr < Formula
-  version 'v0.2.4'
+  version 'v0.3.0'
   desc "Cardano Node on Rust"
   homepage "https://github.com/input-output-hk/jormungandr"
 
   url "https://github.com/input-output-hk/jormungandr/releases/download/#{version}/jormungandr-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "fc6b263ef19c99e18fc79a71c12010f82889847e2306eb8dbd86b96c3d3064b7"
+  sha256 "405de80fd0f78ae5e5bfdedee95c0555cdfd38f54b62156fd970ed409e515ffe"
 
   def install
     bin.install "jormungandr"
